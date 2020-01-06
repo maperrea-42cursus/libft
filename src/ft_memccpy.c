03b14e7d9ee5d5d1ed177896daa3d9abbf429c3b
Matthis Perreaux <maperrea@w-r3-p5.s19.be>
2020-01-06 18:21:08 +0100

    memccpy

--- src/ft_memccpy.c
+++ src/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: maperrea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/06 16:42:37 by maperrea          #+#    #+#             */
-/*   Updated: 2020/01/06 17:41:18 by maperrea         ###   ########.fr       */
+/*   Updated: 2020/01/06 18:20:49 by maperrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ void *memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t i;
 
-	while (i < n && ((unsigned char*)src)[i] != (unsigned char)c)
+	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		if (((unsigned char *)src)[i] = (unsigned char)c)
+			return (&(((unsigned char *)dst)[i + 1])
 		i++;
 	}
+	return (NULL);
 }
